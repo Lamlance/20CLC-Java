2444e4ad472d1b0b5c1f4dd6469be398bb94ccdf
lamlance <lamhoangdien113@gmail.com>
2022-11-16 14:17:18 +0700

    Make a hashmap for slang key and def

--- SlangDict/SlangDictHashMap.java
+++ SlangDict/SlangDictHashMap.java
@@ -1,1 +1,40 @@
+package SlangDict;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SlangDictHashMap {
+  Map<String,String> slangDict = new HashMap<String,String>();
+
+  public boolean readFromFile(String fileName) {
+    try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName))) {
+      String lineData = null;
+      while((lineData = fileReader.readLine()) != null){
+        String[] values = lineData.split("`");
+        if(values.length >= 2){
+          String key = values[0];
+          String def = values[1];
+          this.slangDict.put(key, def);
+        }
+      }
+      return true;
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return false;
+  }
+
+  public String getSlangByKey(String key){
+    return this.slangDict.get(key);
+  }
+
+  public Map<String,String> getSlangDict() {
+    return this.slangDict;
+  }
+
+  public void setSlangDict(Map<String,String> slangDict) {
+    this.slangDict = slangDict;
+  }
+}
